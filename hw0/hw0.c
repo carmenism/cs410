1d60fe006eafce790300bc08092effdf5d207f9a
crstjean <crstjean@localhost>
2014-05-09 23:53:38 -0400

    added assignment 0

---
+++ hw0/hw0.c
@@ -0,0 +1,29 @@
+/**************************************************************
+ )                                                           (
+(   NAME:                     Carmen St. Jean                 )
+ )  SECTION:                  01                             (
+(                                                             )
+ )  ASSIGNMENT:               0                              (
+(   DUE DATE:                 September 12, 2007              )
+ )                                                           (
+(   PURPOSE:                                                  )
+ )     This program will print my name.                      (
+(   INPUT:                                                    )
+ )     Nothing.                                              (
+(   OUTPUT:                                                   )
+ )     My name.                                              (
+(                                                             )
+**************************************************************/
+#include <stdio.h>
+
+int
+main(void)
+{
+     printf("\n   My Name is:  Carmen St. Jean");
+
+     printf("\n\nGoodbye\n");
+     return 0;
+
+}/*END OF MAIN************************************************************/
+
+
