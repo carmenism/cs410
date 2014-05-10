14233b05143c13d32ec81853233aa2e298cd0792
crstjean <crstjean@localhost>
2014-05-09 23:54:37 -0400

    added assignment 1

---
+++ hw1/hw1.c
@@ -0,0 +1,42 @@
+/**************************************************************
+*                                                             *
+*   NAME:                     Carmen St. Jean                 *
+*   SECTION:                  01                              *
+*                                                             *
+*   ASSIGNMENT:               1                               *
+*   DUE DATE:                 September 17, 2007              *
+*                                                             *
+*   PURPOSE:                                                  *
+*      Prints Mickey Mouse.                                   *
+*   INPUT:                                                    *
+*      Nothing.                                               *
+*   OUTPUT:                                                   *
+*      Mickey Mouse.                                          *
+*                                                             *
+**************************************************************/
+#include <stdio.h>
+
+int
+main(void)
+{
+    printf("\n      ***                  ***");
+    printf("\n   *MMMMMMMM            MMMMMMMM*");
+    printf("\n  *MMMMMMMMMM          MMMMMMMMMM*");
+    printf("\n  MMMMMMMMMM*          *MMMMMMMMMM");
+    printf("\n  MMMMMMMMMMM**MMMMMM**MMMMMMMMMMM");
+    printf("\n   *MMMMMMMMMMMMMMMMMMMMMMMMMMMM*");
+    printf("\n      *MMMMMMMMMMMMMMMMMMMMMM*");
+    printf("\n          *MMMMMMMMMMMMMM*");
+    printf("\n         MMMMMMMMMMMMMMMMMM\n");
+    printf("         MMMMMMMMMMMMMMMMMM");    /*This line has no new-line*/ 
+    printf("\n         MMMMMMMMMMMMMMMMMM");
+    printf("\n          MMMMMMMMMMMMMMMM");
+    printf("\n           MMMMMMMMMMMMMM");
+    printf("\n            *MMMMMMMMMM*");
+    printf("\n               *MMMM*\n\n");
+    printf("\n       M-I-C-K-E-Y M-O-U-S-E!\n");
+
+    return 0;
+
+}
+
